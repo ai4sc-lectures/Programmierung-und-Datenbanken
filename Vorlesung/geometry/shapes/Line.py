898d33084eecbd65c65c845d8e57ab09506a5371
Joern Ploennigs <joern.ploennigs@uni-rostock.de>
2022-12-05 12:54:54 +0100

    Update Line.py

--- Vorlesung/geometry/shapes/Line.py
+++ Vorlesung/geometry/shapes/Line.py
@@ -1,6 +1,7 @@
+from geometry.points.ImmutablePoint import ImmutablePoint
 
 class Line:
-    def __init__(self, start: 'ImmutablePoint', end: 'ImmutablePoint'):
+    def __init__(self, start: ImmutablePoint, end: ImmutablePoint):
         self.start = start
         self.end = end
 
