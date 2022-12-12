40b0dc2013535a504e171f25455b40689881d232
Joern Ploennigs <joern.ploennigs@uni-rostock.de>
2022-12-12 09:15:35 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
+rem git submodule update --init --recursive
+rem git submodule update --remote --merge
+rem git submodule foreach "git fetch && git reset --hard origin/main"
 jupyter-book build .
 ghp-import -n -p -f _build/html
 jupyter-book build . --builder pdfhtml
