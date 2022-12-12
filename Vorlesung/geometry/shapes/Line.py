eb32be03f0d32b111c0866014664c04f73fcaf99
Joern Ploennigs <joern.ploennigs@uni-rostock.de>
2022-12-12 13:08:55 +0100

    Update Line.py

--- Vorlesung/geometry/shapes/Line.py
+++ Vorlesung/geometry/shapes/Line.py
@@ -2,6 +2,10 @@ from geometry.points.ImmutablePoint import ImmutablePoint
 
 class Line:
     def __init__(self, start: ImmutablePoint, end: ImmutablePoint):
+        if not isinstance(start, ImmutablePoint):
+            raise TypeError("start not of type ImmutablePoint")
+        if not isinstance(end, ImmutablePoint):
+            raise TypeError("end not of type ImmutablePoint")
         self.start = start
         self.end = end
 
