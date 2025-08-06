b62c9c7d04c7c686c15a6a5aa14b165c71b8782d
Joern Ploennigs <joern.ploennigs@uni-rostock.de>
2025-08-06 11:00:20 +0200

    up

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-jupyter-book build .
-cp Vorlesung\images\*.* _build\html\_images
-ghp-import -n -p -f _build/html
-jupyter-book build . --builder pdfhtml
+#jupyter-book build .
+#cp Vorlesung\images\*.* _build\html\_images
+#ghp-import -n -p -f _build/html
+#jupyter-book build . --builder pdfhtml
+
+uv run invoke build
